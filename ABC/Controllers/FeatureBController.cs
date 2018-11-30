9ade416af854c289b06e57df8809308c370d4199
ttcntt <ttcntt.vnptlamdong@gmail.com>
2018-11-30 20:42:52 +0700

    'featureb'

---
+++ ABC/Controllers/FeatureBController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ABC.Controllers
+{
+    public class FeatureBController : Controller
+    {
+        // GET: FeatureB
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
