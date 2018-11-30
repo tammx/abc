e86deb5b5897c4054afc518d87ef014fb80b2c3c
ttcntt <ttcntt.vnptlamdong@gmail.com>
2018-11-30 20:30:09 +0700

    'featurea'

---
+++ ABC/Controllers/FeatureAController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ABC.Controllers
+{
+    public class FeatureAController : Controller
+    {
+        // GET: FeatureA
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
